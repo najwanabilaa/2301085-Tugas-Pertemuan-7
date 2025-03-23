503b911226216e63bc0a4442044881bd8e0219d9
Najwa Nabila H <162705772+najwanabilaa@users.noreply.github.com>
2025-03-23 20:49:41 +0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.oopjava.enkapsulasi;
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai a: ");
+        double a = input.nextDouble();
+        System.out.print("Masukkan nilai b: ");
+        double b = input.nextDouble();
+        System.out.print("Masukkan nilai c: ");
+        double c = input.nextDouble();
+        System.out.print("Masukkan nilai d: ");
+        double d = input.nextDouble();
+        System.out.print("Masukkan nilai e: ");
+        double e = input.nextDouble();
+        System.out.print("Masukkan nilai f: ");
+        double f = input.nextDouble();
+
+        Enkapsulasi persamaan = new Enkapsulasi(a, b, c, d, e, f);
+
+  
+        if (persamaan.isSolvable()) {
+            System.out.println("Nilai x = " + persamaan.getX());
+            System.out.println("Nilai y = " + persamaan.getY());
+        } else {
+            System.out.println("Persamaan tidak memiliki solusi.");
+        }
+
+        input.close();
+    }
+}
+
